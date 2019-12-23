721447f4b70db5c10af719303b45629dc2067408
彭宽旺 <kuanwang.peng@huifu.com>
2019-12-23 18:27:06 +0800

    activity之间的传值

--- app/src/main/java/com/huifu/myapplication/SimpleActivity.kt
+++ app/src/main/java/com/huifu/myapplication/SimpleActivity.kt
@@ -3,6 +3,7 @@ package com.huifu.myapplication
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
+import kotlinx.android.synthetic.main.activity_simple.*
 
 /**
  * @author pengkuanwang
@@ -14,8 +15,8 @@ class SimpleActivity : BaseActivity() {
     @Autowired
     lateinit var name: String
 
-    @Autowired
-    lateinit var name1: String
+    @Autowired(name = "name1")
+    lateinit var aliasName: String
 
     @Autowired
     lateinit var person: Person
@@ -23,7 +24,10 @@ class SimpleActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
-        println("$name,$name1")
-        println(person.name)
+        textView.append("name:$name")
+        textView.append("\n\n")
+        textView.append("name:$aliasName")
+        textView.append("\n\n")
+        textView.append("name3:${person.name},年龄:$person.age")
     }
 }
